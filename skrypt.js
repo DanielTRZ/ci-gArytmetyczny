1dce7cc540363e1fc39b081fe9e87787897e991d
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2022-05-31 13:37:41 +0200

    Add files via upload

---
+++ skrypt.js
@@ -0,0 +1,17 @@
+function generuj(){
+
+var a = document.getElementById("wyraz1").value;
+   // a = parseInt("wyraz1");
+var b = document.getElementById("wyraz2").value;
+   // b = parseInt("wyraz2");
+var c = document.getElementById("wyraz3").value;
+   // c = parseInt("wyraz3");
+var d ="";
+
+for( i=1; i <= c; i++){
+    
+  d+=a*1+(i-1)*b+", ";    
+ 
+}
+document.getElementById("wynik").innerHTML=d +" < koniec ciągu";
+}
